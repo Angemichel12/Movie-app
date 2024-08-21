48638fd41ec915237678f7c785c03ea8ae63299e
Angemichel12 <angemicheluwizeye@gmail.com>
2024-08-21 18:47:45 +0200

    routes error handling

---
+++ src/@types/errorType.ts
@@ -0,0 +1,5 @@
+export type errType ={
+    message:String,
+    status:String,
+    statusCode:Number
+}
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import apiRouter from './routes/index';
 import dotenv from 'dotenv/config';
+import { errType } from './@types/errorType';
 const app = express();
 
 app.use(express.json());
@@ -10,5 +11,15 @@ app.use('/api/v1', apiRouter);
 app.use('/api/v1', (req, res) => {
   res.status(200).json({ message: 'Welcome to the movie API' });
 });
-
+app.all('*', (req:Request, res:Response, next:NextFunction)=>{
+const err = new Error(`Can't find ${req.originalUrl} on the server!`) as any;
+err.status = 'Not Found';
+err.statusCode = 404;
+next(err as errType)
+})
+app.use((err:errType,req:Request,Res:Response, next:NextFunction)=>{
+  err.status = err.status || "Error";
+  err.statusCode = err.statusCode || 500;
+  Res.status(err.statusCode as number).json({status:err.status, message:err.message})
+})
 export default app;
